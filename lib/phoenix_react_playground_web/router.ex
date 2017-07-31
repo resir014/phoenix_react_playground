dbfadb4f8403337436e639217e30b1bb30452be9
Resi Respati <resir014@gmail.com>
2017-07-31 11:50:59 +0700

    Set up Phoenix routes and views to the React app

--- lib/phoenix_react_playground_web/router.ex
+++ lib/phoenix_react_playground_web/router.ex
@@ -16,7 +16,7 @@ defmodule PhoenixReactPlaygroundWeb.Router do
   scope "/", PhoenixReactPlaygroundWeb do
     pipe_through :browser # Use the default browser stack
 
-    get "/", PageController, :index
+    get "/*path", PageController, :index
   end
 
   # Other scopes may use custom stacks.
